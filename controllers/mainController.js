70a141666b9f663f655f35114c6beb9bb5ccc888
javi-dh <javier@digitalhouse.com>
2019-11-27 20:29:21 -0300

    Little changes in package.json and mainControlle.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 // ************ Function to Read an HTML File ************
 function readHTML (fileName) {
-	let htmlFile = fs.readFileSync(path.join(__dirname, `/../views/${fileName}.html`), 'utf-8');
+	let filePath = path.join(__dirname, `/../views/${fileName}.html`);
+	let htmlFile = fs.readFileSync(filePath, 'utf-8');
 	return htmlFile;
 }
 
@@ -11,7 +12,7 @@ const controller = {
 	root: (req, res) => {
 		let html = readHTML('index');
 		res.send(html);
-	}
+	},
 };
 
 module.exports = controller
